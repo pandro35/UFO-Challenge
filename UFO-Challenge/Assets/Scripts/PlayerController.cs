d04196d6a08b44318defd0e51845fc993a161e01
pandro35 <55207319+pandro35@users.noreply.github.com>
2019-09-22 18:48:36 -0400

    UFO Challenge 1

---
+++ UFO-Challenge/Assets/Scripts/PlayerController.cs
@@ -0,0 +1,92 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class PlayerController : MonoBehaviour
+{
+    public float speed;
+    public Text countText;
+    public Text winText;
+    public Text livesText;
+    public Text loseText;
+    private Rigidbody2D rb2d;
+    private int count;
+    private int lives;
+
+    void Start()
+    {
+        rb2d = GetComponent<Rigidbody2D>();
+        count = 0;
+        lives = 3;
+        winText.text = "";
+        loseText.text = "";
+        SetCountText();
+        SetLivesText();
+    }
+    void FixedUpdate()
+    {
+        float moveVertical = Input.GetAxis("Vertical");
+        float moveHorizontal = Input.GetAxis("Horizontal");
+        Vector2 movement = new Vector2(moveHorizontal, moveVertical);
+        rb2d.AddForce(movement * speed);
+    }
+
+    void OnTriggerEnter2D(Collider2D other)
+    {
+        if (other.gameObject.CompareTag("Pickup"))
+        {
+            other.gameObject.SetActive(false);
+            if (lives > 0)
+            {
+                count = count + 1;
+            }
+            SetCountText();
+        }
+        else if (other.gameObject.CompareTag("Enemy"))
+        {
+            other.gameObject.SetActive(false);
+            if(count < 20 && lives != 0)
+            {
+                lives = lives - 1;
+            }
+            SetLivesText();
+        }
+    }
+
+    void SetCountText()
+    {
+        countText.text = "Count: " + count.ToString();
+        if (count >= 20)
+        {
+            winText.text = "You win! Game Created by Alejandro Porras";
+            
+        }
+        else if (count == 12)
+        {
+            transform.position = new Vector2(82.1f, 10.75f);
+        }
+    }
+
+    void SetLivesText()
+    {
+        livesText.text = "Lives: " + lives.ToString();
+        if (lives == 0)
+        {
+            DestroyGameObject(this);
+            loseText.text = "You Lose! Game Created by Alejandro Porras";
+        }
+    }
+    void Update()
+    {
+        if (Input.GetKey("escape"))
+        {
+            Application.Quit();
+        }
+
+    }
+    void DestroyGameObject(PlayerController playerController)
+    {
+        Destroy(gameObject);
+    }
+}
